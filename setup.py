eddfef1046a89c4c0eedcf5e5aef11352e1882f4
Russ Holloway <rholloway@paloaltonetworks.com>
2016-02-09 19:58:00 -0800

    Unique name across repos to avoid egg link collision

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 
-    name            =   "GSRT",
+    name            =   "AutofocusClientLibrary",
     packages        =   ['autofocus'],
     version         =   '0.1.2',
     description     =   'AutoFocus Client Lib',
