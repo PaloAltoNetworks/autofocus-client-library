f24257da919982fcb0dbb667a460474c505ed32f
Benjamin Small <bsmall@paloaltonetworks.com>
2016-06-24 07:26:41 -0700

    new session attributes in service response

--- autofocus/autofocus.py
+++ autofocus/autofocus.py
@@ -751,46 +751,46 @@ class AFSession(AutoFocusObject):
         self.application = kwargs.get("app")
 
         #: str: The account name for the device (regular users will only see their account)
-        self.account_name = kwargs.get("device.acctname")
+        self.account_name = kwargs.get("device_acctname")
 
         #: str: The country code where the device detecting the activity exists
-        self.device_country_code = kwargs.get("device.countrycode")
+        self.device_country_code = kwargs.get("device_countrycode")
 
         #: str: The country where the device detecting the activity exists
-        self.device_country = kwargs.get("device.country")
+        self.device_country = kwargs.get("device_country")
 
         #: str: The hostname of the device detecting the activity
-        self.device_hostname = kwargs.get("device.hostname")
+        self.device_hostname = kwargs.get("device_hostname")
 
         #: str: The business industry that the activity was detected on
-        self.industry = kwargs.get("device.industry")
+        self.industry = kwargs.get("device_industry")
 
         #: str: The line of business that the activity was detected on
-        self.business_line = kwargs.get("device.lob")
+        self.business_line = kwargs.get("device_lob")
 
         #: str: The model of the device reporting the activity
-        self.device_model = kwargs.get("device.model")
+        self.device_model = kwargs.get("device_model")
 
         #: str: The serial number of the device reporting activity
-        self.device_serial = kwargs.get("device.serial")
+        self.device_serial = kwargs.get("device_serial")
 
         #: str: The version of the device reporting activity
-        self.device_version = kwargs.get("device.swver")
+        self.device_version = kwargs.get("device_swver")
 
         #: str: The country code of the destination
-        self.dst_country_code = kwargs.get("dst.countrycode")
+        self.dst_country_code = kwargs.get("dst_countrycode")
 
         #: str: The country of the destination
-        self.dst_country = kwargs.get("dst.country")
+        self.dst_country = kwargs.get("dst_country")
 
         #: str: The destination IP address
-        self.dst_ip = kwargs.get("dst.ip")
+        self.dst_ip = kwargs.get("dst_ip")
 
         #: bool: true/false whether the IP is private
         self.dst_is_private_ip = True if kwargs.get("dst.isprivateip") else False
 
         #: int: the destination port of the activity
-        self.dst_port = kwargs.get("dst.port")
+        self.dst_port = kwargs.get("dst_port")
 
         #: str: the destination address(es) of the email
         self.email_recipient = kwargs.get("emailrecipient")
@@ -817,19 +817,19 @@ class AFSession(AutoFocusObject):
         self.sha256 = kwargs.get("sha256")
 
         #: str: The country code of the source
-        self.src_country_code = kwargs.get("src.countrycode")
+        self.src_country_code = kwargs.get("src_countrycode")
 
         #: str: The country of the source
-        self.src_country = kwargs.get("src.country")
+        self.src_country = kwargs.get("src_country")
 
         #: str: The source IP address
-        self.src_ip = kwargs.get("src.ip")
+        self.src_ip = kwargs.get("src_ip")
 
         #: bool: true/false whether the IP is private
-        self.src_is_private_ip = True if kwargs.get("src.isprivateip") else False
+        self.src_is_private_ip = True if kwargs.get("src_isprivateip") else False
 
         #: int: the destination port of the activity
-        self.src_port = kwargs.get("src.port")
+        self.src_port = kwargs.get("src_port")
 
         timestamp = kwargs.get("tstamp")
         self._raw_timestamp = timestamp
