1e7af3e3abd2732dd5837b93aa7fb3ac3cd51e81
Russ Holloway <rholloway@paloaltonetworks.com>
2016-12-20 11:16:57 -0500

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 
     name            =   "AutofocusClientLibrary",
     packages        =   ['autofocus'],
-    version         =   '1.0.22',
+    version         =   '1.0.23',
     description     =   'AutoFocus Client Lib',
     author          =   'Ben Small, Pat Litke, Russ Holloway, GSRT',
     author_email    =   'gsrt-tech@paloaltonetworks.com',
