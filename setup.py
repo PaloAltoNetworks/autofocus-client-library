61a2e89b6575b48b7b1fd680ea13b56cf884683a
Russ Holloway <rholloway@paloaltonetworks.com>
2016-12-21 09:48:54 -0500

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python2.7
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
 
     name            =   "AutofocusClientLibrary",
     packages        =   ['autofocus'],
-    version         =   '1.0.23',
+    version         =   '1.0.24',
     description     =   'AutoFocus Client Lib',
     author          =   'Ben Small, Pat Litke, Russ Holloway, GSRT',
     author_email    =   'gsrt-tech@paloaltonetworks.com',
